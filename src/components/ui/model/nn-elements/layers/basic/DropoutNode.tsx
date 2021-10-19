5183052677dc6dfe7cc7ffea0b8dede9e6089ca9
Lars <larslpt@gmail.com>
2021-10-19 16:49:08 +0200

    Select Input Focus Manually

--- src/components/ui/model/nn-elements/layers/basic/DropoutNode.tsx
+++ src/components/ui/model/nn-elements/layers/basic/DropoutNode.tsx
@@ -13,6 +13,118 @@ interface DropoutArgs extends NodeLayerArgs{
     noiseShape: number[] | undefined; 
     seed: number | undefined;
 }
+
+const DenseNode = (props: NodeProps<DataBaseType>) => {
+  const [rateArg, setRateArg] = useState<number>();
+
+  useEffect(() => {
+    if (props.data.outputValue === undefined) {
+      setOutputShape("");
+    }
+  }, [props.data]);
+
+  const layerFunction = (input: layerOutput | undefined) => {
+    if (input === undefined) return input;
+
+    const name = nameArg.length === 0 ? undefined : nameArg;
+    const units = unitsArg;
+    const useBias = useBiasArg;
+    const activation = getActivation(activationArg);
+    const kernelInitializer = getInitializer(kernelInitializerArg);
+    const biasInitializer = getInitializer(biasInitializerArg);
+    const kernelConstraint = getConstraint(kernelConstraintArg);
+    const biasConstraint = getConstraint(biasConstraintArg);
+    const kernelRegularizer = getRegularizer(kernelRegularizerArg);
+    const biasRegularizer = getRegularizer(biasRegularizerArg);
+    const ret = dense({
+      name: name,
+      units: units,
+      activation: activation,
+      useBias: useBias,
+      kernelInitializer: kernelInitializer,
+      biasInitializer: biasInitializer,
+      kernelConstraint: kernelConstraint,
+      biasConstraint: biasConstraint,
+      kernelRegularizer: kernelRegularizer,
+      biasRegularizer: biasRegularizer,
+    } as DenseLayerArgs).apply(input.layerOutput) as SymbolicTensor;
+
+    console.log("output layer dense: ", ret, ret.shape);
+    setOutputShape(ret.shape.slice(1).join("x"));
+
+    return { layerOutput: ret, modelInput: input.modelInput } as layerOutput;
+  };
+
+  // may need some other performance optimization
+
+  useEffect(() => {
+    props.data.changed = true;
+  }, [unitsArg]);
+
+  // portals dont work well with focus
+  // so we have to implement our own focus logic
+  useEffect(() => {
+    if (focusRef.current) {
+      focusRef.current.focus();
+    }
+  });
+
+  return (
+    <BaseNode
+      {...props}
+      menu={
+        <ArgsMenu>
+          <Typography variant="h4" mt={2}>
+            options
+          </Typography>
+
+          <TextInput<string>
+            name="name"
+            value={nameArg}
+            setValue={setNameArg}
+            ref={focused === "name" ? focusRef : null}
+            onFocus={() => {
+              setFocused("name");
+              console.log("focus name");
+            }}
+          />
+          <Divider />
+          <TextInput<number>
+            number
+            name="units"
+            value={unitsArg}
+            setValue={setUnitsArg}
+            ref={focused === "units" ? focusRef : null}
+            onFocus={() => {
+              setFocused("units");
+              console.log("focus units");
+            }}
+          />
+          <Divider />
+          <SelectInput
+            value={activationArg}
+            setValue={(value: string) => {
+              setActivationArg(ACTIVATIONS[value as keyof typeof ACTIVATIONS]);
+            }}
+            name="activation"
+            options={Object.keys(ACTIVATIONS)}
+            ref={focused === "activation" ? focusRef : null}
+            onFocus={() => {
+              setFocused("activation");
+              console.log("focus activation");
+            }}
+          />
+          <Divider />
+        </ArgsMenu>
+      }
+      layerFunction={layerFunction}
+      layerTypeName="dense"
+    >
+      {}
+    </BaseNode>
+  );
+};
+
 /*
 const getDropoutLayerFunction = (args: DropoutArgs) => {
   return (input: layerOutput | undefined) => {
