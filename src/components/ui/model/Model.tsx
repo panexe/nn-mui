a9769b315eca0f32bbc63610f52d0f2316087c48
Lars <larslpt@gmail.com>
2021-11-01 13:38:31 +0100

    Cleanup Files

--- src/components/ui/model/Model.tsx
+++ src/components/ui/model/Model.tsx
@@ -2,7 +2,6 @@
 import ThreeColLayout from "../../layout/three-col-layout/ThreeColLayout";
 import Sidebar from "./sidebar/Sidebar";
 import NetworkEditor from "./network-editor/NetworkEditor";
-import ModelContextProvider from "../../context-providers/ModelContextProvider";
 import { ReactFlowProvider } from "react-flow-renderer";
 import { Portals } from "../../../types";
 import theme from "../../../theme";
@@ -11,22 +10,20 @@ import { TensorflowAdapter } from "../../../adapters/INNLib";
 const Model = () => {
   return (
     <ReactFlowProvider>
-      <ModelContextProvider>
-        <ThreeColLayout
-          leftContent={<Sidebar />}
-          mainContent={<NetworkEditor lib={new TensorflowAdapter()} />}
-          rightContent={
-            <div
-              id={Portals.layerInfo}
-              style={{
-                backgroundColor: theme.palette.action.hover,
-                height: "100%",
-                overflow: "auto",
-              }}
-            ></div>
-          }
-        />
-      </ModelContextProvider>
+      <ThreeColLayout
+        leftContent={<Sidebar />}
+        mainContent={<NetworkEditor lib={new TensorflowAdapter()} />}
+        rightContent={
+          <div
+            id={Portals.layerInfo}
+            style={{
+              backgroundColor: theme.palette.action.hover,
+              height: "100%",
+              overflow: "auto",
+            }}
+          ></div>
+        }
+      />
     </ReactFlowProvider>
   );
 };
