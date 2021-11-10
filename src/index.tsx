dbb2f8d3b6f7345f7c6bc2470da1195cc6e3ca16
Lars <larslpt@gmail.com>
2021-11-10 14:58:14 +0100

    Redesigned UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import * as React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
-import theme from "./theme";
 import { Provider } from "react-redux";
 import store from "./store";
+import "@fontsource/roboto-mono";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <App />
   </Provider>,
   document.querySelector("#root")
 );
